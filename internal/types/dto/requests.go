22cf072ed9e48ba32c0d1816c59fbfd52bdcef43
deAd-otAku327 <almchech@mail.ru>
2025-08-10 21:23:38 +0300

    improved validation rules

--- internal/types/dto/requests.go
+++ internal/types/dto/requests.go
@@ -2,7 +2,7 @@ package dto
 
 type CreateSubscriptionRequest struct {
 	ServiceName string  `json:"service_name"`
-	Price       int     `json:"price" validate:"required,gt=0"`
+	Price       int     `json:"price" validate:"required,gte=0"`
 	UserID      string  `json:"user_id" validate:"required,uuid4"`
 	StartDate   string  `json:"start_date" validate:"required,datetime=01-2006"`
 	EndDate     *string `json:"end_date" validate:"omitempty,datetime=01-2006"`
@@ -14,8 +14,8 @@ type GetSubscriptionRequest struct {
 
 type UpdateSubscriptionRequest struct {
 	SubID   string  `validate:"required,uuid4"` // FROM PATH.
-	Price   *int    `json:"price" validate:"omitempty,gte=0"`
-	EndDate *string `json:"end_date" validate:"omitempty,datetime=01-2006"`
+	Price   *int    `json:"price" validate:"required_without=EndDate,gte=0"`
+	EndDate *string `json:"end_date" validate:"required_without=Price,datetime=01-2006"`
 }
 
 type DeleteSubscriptionRequest struct {
@@ -23,13 +23,13 @@ type DeleteSubscriptionRequest struct {
 }
 
 type ListSubscriptionsRequest struct {
-	UserID      *string `schema:"user_id" validate:"omitempty,uuid4"`
-	ServiceName *string `schema:"service"`
+	UserID      *string `schema:"user_id" validate:"required_without=ServiceName,uuid4"`
+	ServiceName *string `schema:"service" validate:"required_without=UserID"`
 }
 
 type GetTotalCostRequest struct {
 	FromDate    string  `schema:"from" validate:"required,datetime=01-2006"`
 	ToDate      string  `schema:"to" validate:"required,datetime=01-2006"`
-	UserID      *string `schema:"user_id" validate:"omitempty,uuid4"`
-	ServiceName *string `schema:"service"`
+	UserID      *string `schema:"user_id" validate:"required_without=ServiceName,uuid4"`
+	ServiceName *string `schema:"service" validate:"required_without=UserID"`
 }
